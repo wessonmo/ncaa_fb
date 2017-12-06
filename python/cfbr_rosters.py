aa72df8a13fa1c0da0c80288eebe37d96712bc35
wessonmo <wessonmo@gmail.com>
2017-12-05 22:36:12 -0600

    remove duplicates, +mod_name

--- python/cfbr_rosters.py
+++ python/cfbr_rosters.py
@@ -31,86 +31,83 @@ except:
     
 try:
     rosters = pd.read_csv('csv\\rosters_cfbr.csv', header = 0)
+    hdr = False
 except:
-#    rosters = pd.DataFrame(columns = ['season', 'team', 'player', 'mod_name', 'starter', 'href', 'class', 'pos'])
+    rosters = pd.DataFrame(columns = ['season', 'team', 'player', 'mod_name', 'href', 'class', 'pos'])
+    hdr = True
     
-    for season in range(2009,2018):
-        for index, row in team_hrefs.iterrows():
-            if len(rosters.loc[(rosters['team'] == row['name']) & (rosters['season'] == season),]) < 1:
-                team = pd.DataFrame(columns = ['season', 'team', 'player', 'mod_name', 'starter', 'href', 'class', 'pos'])
-                
-                roster_url = 'https://www.sports-reference.com' + row['href'] + str(season) + '-roster.html'
-                roster_req = requests.get(roster_url, headers = header)
-                roster_soup = BeautifulSoup(roster_req.content, 'lxml')
-                
-                if pd.isnull(row['nickname']):
-                    try:
-                        nickname = roster_soup.find('h1', {'itemprop': 'name'}).contents[5].text
-                        team_hrefs.set_value(index,'nickname',nickname)
-                    except:
-                        continue
-                
+for season in range(2009,2018):
+    for index, row in team_hrefs.iterrows():
+        if len(rosters.loc[(rosters['team'] == row['name']) & (rosters['season'] == season),]) < 1:
+            team = pd.DataFrame(columns = ['season', 'team', 'player', 'mod_name', 'href', 'class', 'pos'])
+            
+            roster_url = 'https://www.sports-reference.com' + row['href'] + str(season) + '-roster.html'
+            roster_req = requests.get(roster_url, headers = header)
+            roster_soup = BeautifulSoup(roster_req.content, 'lxml')
+            
+            if pd.isnull(row['nickname']):
                 try:
-                    players = roster_soup.find('table', {'id': 'roster'}).find('tbody').find_all('tr', {'class': None})
+                    nickname = roster_soup.find('h1', {'itemprop': 'name'}).contents[5].text
+                    team_hrefs.set_value(index,'nickname',nickname)
                 except:
                     continue
-                for player in players:
-                    raw_name = player.contents[0].text[:-1] if player.contents[0].text[-1] == '*' else player.contents[0].text
-                    mod_name = re.sub(remove_char,'',raw_name.lower())
-    
-                    starter = 1 if player.contents[0].text[-1] == '*' else 0
-                    try:
-                        href = player.contents[0].contents[0].get('href')
-                    except:
-                        href = None
-                    class_ = None if player.contents[1].text == '' else player.contents[1].text
-                    pos = None if player.contents[2].text == '' else player.contents[2].text
-        
-                    team.loc[len(team),] = [season, row['name'], raw_name, mod_name, starter, href, class_, pos]
-                
-                for index2, row2 in team.loc[pd.isnull(team['href'])].iterrows():
-                    match = process.extractOne(row2['mod_name'], team.loc[(pd.isnull(team['href']) == False),'mod_name'])
-                    if match[1] > 90:
-                        team.drop(index2, inplace = True)
-                        
-                team = team.reset_index(drop = True)
-                        
-                for index3, row3 in team.loc[pd.isnull(team['href']) == False].iterrows():
-                    other_players = team.loc[(pd.isnull(team['href']) == False) & (team['href'] != row3['href']),]
-                    match = process.extractOne(row3['mod_name'], list(other_players['mod_name']))
-                    if match[1] > 90:
-                        similar = team.loc[(team['href'] != row3['href']) & (team['mod_name'] == match[0])].iloc[0]
-                        if (row3['pos'] == similar['pos']) | (match[1] == 100):
-                            player1_url = 'https://www.sports-reference.com' + row3['href']
-                            player1_req = requests.get(player1_url, headers = header)
-                            player1_soup = BeautifulSoup(player1_req.content, 'lxml')
-                            player1_years = len(player1_soup.find_all('th', {'data-stat': 'year_id'}))
-                            
-                            player2_url = 'https://www.sports-reference.com' + similar['href']
-                            player2_req = requests.get(player2_url, headers = header)
-                            player2_soup = BeautifulSoup(player2_req.content, 'lxml')
-                            player2_years = len(player2_soup.find_all('th', {'data-stat': 'year_id'}))
-                            
-                            if player1_years >= player2_years:
-                                try:
-                                    team.drop(similar.name, inplace = True)
-                                except:
-                                    continue
-                            else:
-                                try:
-                                    team.drop(index3, inplace = True)
-                                except:
-                                    continue
-                                
-#                rosters = pd.concat([rosters,team]).drop_duplicates().reset_index(drop = True)
-                with open('my_csv.csv', 'ab') as f:
-                    team.to_csv('csv\\rosters_cfbr.csv', index = False)
-                print(season, team['team'].iloc[0])
-                        
-                        
+            
+            try:
+                players = roster_soup.find('table', {'id': 'roster'}).find('tbody').find_all('tr', {'class': None})
+            except:
+                continue
+            for player in players:
+                raw_name = player.contents[0].text[:-1] if player.contents[0].text[-1] == '*' else player.contents[0].text
+                mod_name = re.sub(remove_char,'',raw_name.lower())
 
+                try:
+                    href = player.contents[0].contents[0].get('href')
+                except:
+                    href = None
+                class_ = None if player.contents[1].text == '' else player.contents[1].text
+                pos = None if player.contents[2].text == '' else player.contents[2].text
+    
+                team.loc[len(team),] = [season, row['name'], raw_name, mod_name, href, class_, pos]
+            
+            for index2, row2 in team.loc[pd.isnull(team['href'])].iterrows():
+                match = process.extractOne(row2['mod_name'], team.loc[(pd.isnull(team['href']) == False),'mod_name'])
+                if match[1] > 90:
+                    team.drop(index2, inplace = True)
+                    
+            team = team.reset_index(drop = True)
+                    
+            for index3, row3 in team.loc[pd.isnull(team['href']) == False].iterrows():
+                other_players = team.loc[(pd.isnull(team['href']) == False) & (team['href'] != row3['href']),]
+                match = process.extractOne(row3['mod_name'], list(other_players['mod_name']))
+                if match[1] > 90:
+                    similar = team.loc[(team['href'] != row3['href']) & (team['mod_name'] == match[0])].iloc[0]
+                    if (row3['pos'] == similar['pos']) | (match[1] == 100):
+                        player1_url = 'https://www.sports-reference.com' + row3['href']
+                        player1_req = requests.get(player1_url, headers = header)
+                        player1_soup = BeautifulSoup(player1_req.content, 'lxml')
+                        player1_years = len(player1_soup.find_all('th', {'data-stat': 'year_id'}))
+                        
+                        player2_url = 'https://www.sports-reference.com' + similar['href']
+                        player2_req = requests.get(player2_url, headers = header)
+                        player2_soup = BeautifulSoup(player2_req.content, 'lxml')
+                        player2_years = len(player2_soup.find_all('th', {'data-stat': 'year_id'}))
+                        
+                        if player1_years >= player2_years:
+                            try:
+                                team.drop(similar.name, inplace = True)
+                            except:
+                                continue
+                        else:
+                            try:
+                                team.drop(index3, inplace = True)
+                            except:
+                                continue
+                            
+            with open('csv\\rosters_cfbr.csv', 'ab') as f:
+                team.to_csv(f, index = False, header = hdr)
+            hdr = False
+            print(season, team['team'].iloc[0])
                         
 team_hrefs.loc[:,'id'] = team_hrefs.sort_values('name').reset_index(drop = True).index
 
 team_hrefs.to_csv('csv\\team_info_cfbr.csv', index = False)
-#rosters.drop_duplicates().to_csv('csv\\rosters_cfbr.csv', index = False)
