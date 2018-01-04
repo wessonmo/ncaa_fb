e97f9914c6e1b78faf37270ac5f37cc5d6e20f1b
wessonmo <wessonmo@gmail.com>
2018-01-04 14:32:03 -0600

    removed modifications that were moved to pbp

--- espn/espn_pbp_modifications.py
+++ espn/espn_pbp_modifications.py
@@ -9,67 +9,20 @@ import csv
 pbp_folder = 'E:\\college football\\pbp'
 
 pbp = pd.read_csv(pbp_folder + '\\csv\\' + 'espn_pbp.csv', header = 0)
-playtype_df = pd.read_csv('espn\\csv\\espn_playtype_group.csv', header = 0)
-pbp = pd.merge(pbp,playtype_df,how = 'left', on = 'playtype')
-pbp['fumble'] = pbp.text.apply(lambda x: 1 if re.compile('fumble', re.I).search(x) else 0)
-pbp['interception'] = pbp.playtype.apply(lambda x: 1 if 'Interception' in x else 0)
+#playtype_df = pd.read_csv('espn\\csv\\espn_playtype_group.csv', header = 0)
+#pbp = pd.merge(pbp,playtype_df,how = 'left', on = 'playtype')
 
-for index, row in pbp.iterrows():
-    if not pd.isnull(row.text):
-        if len(re.findall('penalty',row.text.lower())) > 1:
-            print(row.text)
-            raise Exception
-        prior, post = None, None
-        if re.compile('penalty', re.I).search(row.text):
-            pen_index = row.text.lower().index('penalty')
-            
-            if pen_index not in [0,1]:
-                if re.compile('[A-Z]').search(row.text[pen_index - 2]):
-                    prior = re.compile('[A-Z]+(( )*[A-Z])*(?= (penalty|PENALTY|Penalty))').search(row.text).group(0)
-            if pen_index not in [len(row.text) - 9, len(row.text) - 8]:
-                if re.compile('[A-Z]').search(row.text[pen_index + 2]):
-                    post = re.compile('(?<=(penalty|PENALTY|Penalty) )[A-Z]+(( )*[A-Z])*').search(row.text).group(0)
-                    
-            if (prior != None) and (post != None):
-                prior_match = process.extractOne()
-                post_match = process.extractOne()
-        
+drives = list(pbp.driveid.drop_duplicates())
+for drive in drives:
+    plays = pbp.loc[pbp.driveid == drive]
 
-for index, row in pbp.loc[pd.isnull(pbp.playtype_group)].iterrows():
-    if row.playtype == 'Safety':
-        try:
-            if re.compile(' pass | sack', re.I).search(row.text):
-                pbp.set_value(index,'playtype_group','pass')
-            elif re.compile('kickoff', re.I).search(row.text):
-                pbp.set_value(index,'playtype_group','kickoff')
-            elif re.compile(' punt', re.I).search(row.text):
-                pbp.set_value(index,'playtype_group','punt')
-            elif (re.compile(' penalty', re.I).search(row.text)) and (not re.compile('decline').search(row.text)):
-                pbp.set_value(index,'playtype_group','penalty')
-            else:
-                pbp.set_value(index,'playtype_group','safety')
-        except:
-            pbp.set_value(index,'playtype_group','safety')
-    elif 'Fumble' in row.playtype:
-        try:
-            if re.compile(' pass | sack', re.I).search(row.text):
-                pbp.set_value(index,'playtype_group','pass')
-            elif re.compile('kickoff', re.I).search(row.text):
-                pbp.set_value(index,'playtype_group','kickoff')
-            else:
-                pbp.set_value(index,'playtype_group','fumble')
-        except:
-            pbp.set_value(index,'playtype_group','fumble')
-        pbp.set_value(index,'fumble',1)
 
-pbp.loc[pbp.playtype_group.isin(['pass','rush']),'dist'].value_counts().head(40)
-pbp.loc[pbp.playtype_group.isin(['kickoff']),'yrdline'].value_counts()
-        
-for text in pbp.loc[(pbp.dist == 0) & (~pbp.playtype_group.isin(['kickoff','extra point','penalty','end half'])),'text'].head(25):
-    print(text)
-    
-    pbp.loc[pbp.driveid == driveid].iloc[7].text
+pbp.loc[(pbp.playtype == 'pass') & (pbp.fumble == 1)].head()
 
+pbp.playtype.value_counts()
+pbp.loc[pbp.fumble == 1].playtype.value_counts()
+float(499617)/float(540668)
+float(5877)/float(6832)
 
 for index, row in pbp.iterrows():
     drive_plays = pbp.loc[pbp.driveid == row.driveid]
